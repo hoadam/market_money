0854ff219e84eba6af83ce3555487ffd6cec7b8c
Hoa Dam <hoangocdam@gmail.com>
2024-03-25 15:04:47 -0700

    add:show to vendor routes

--- config/routes.rb
+++ config/routes.rb
@@ -11,7 +11,7 @@ Rails.application.routes.draw do
   namespace :api do
     namespace :v0 do
       resources :markets
-      resources :vendors
+      resources :vendors, only: [:show]
     end
   end
 end
