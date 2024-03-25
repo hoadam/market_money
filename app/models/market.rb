fd61ee7bf8576a3e32f1032894327e7d8aa25633
Laura Vega Vargas <ing.laurarvega@gmail.com>
2024-03-25 17:53:11 -0600

    Feat: create market model with its relations, validations and vendor_count instance method

--- app/models/market.rb
+++ app/models/market.rb
@@ -1,3 +1,17 @@
 class Market < ApplicationRecord
+  has_many :market_vendors
+  has_many :vendors, through: :market_vendors
 
+  validates_presence_of :name,
+                        :street,
+                        :city,
+                        :county,
+                        :state,
+                        :zip,
+                        :lat,
+                        :lon
+
+  def vendor_count
+    vendors.count
+  end
 end
