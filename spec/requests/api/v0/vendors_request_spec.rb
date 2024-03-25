68246fe5a9b5b90af35d583d62b8d735907d1adf
Hoa Dam <hoangocdam@gmail.com>
2024-03-25 16:22:33 -0700

    feat:sad path test for story 2:

--- spec/requests/api/v0/vendors_request_spec.rb
+++ spec/requests/api/v0/vendors_request_spec.rb
@@ -8,24 +8,36 @@ describe "Vendors API" do
 
     expect(response).to be_successful
 
-    vendor = JSON.parse(response.body, symbolize_names: true)
+    vendor = JSON.parse(response.body, symbolize_names: true)[:data]
 
-    expect(vendor).to have_key(:id)
-    expect(vendor[:id]).to eq(id)
+    expect(vendor[:attributes]).to have_key(:name)
+    expect(vendor[:attributes][:name]).to be_a(String)
 
-    expect(vendor).to have_key(:name)
-    expect(vendor[:name]).to be_a(String)
+    expect(vendor[:attributes]).to have_key(:description)
+    expect(vendor[:attributes][:description]).to be_a(String)
 
-    expect(vendor).to have_key(:description)
-    expect(vendor[:description]).to be_a(String)
+    expect(vendor[:attributes]).to have_key(:contact_name)
+    expect(vendor[:attributes][:contact_name]).to be_a(String)
 
-    expect(vendor).to have_key(:contact_name)
-    expect(vendor[:contact_name]).to be_a(String)
+    expect(vendor[:attributes]).to have_key(:contact_phone)
+    expect(vendor[:attributes][:contact_phone]).to be_a(String)
 
-    expect(vendor).to have_key(:contact_phone)
-    expect(vendor[:contact_phone]).to be_a(String)
+    expect(vendor[:attributes]).to have_key(:credit_accepted)
+    expect(vendor[:attributes][:credit_accepted]).to be_in([true, false])
+  end
 
-    expect(vendor).to have_key(:credit_accepted)
-    expect(vendor[:credit_accepted]).to be_in([true, false])
+  describe 'sad paths' do
+    it "will gracefully handle if a vendor id doesn't exist" do
+      get "/api/v0/vendors/123123123123"
+
+      expect(response).to_not be_successful
+      expect(response.status).to eq(404)
+
+      data = JSON.parse(response.body, symbolize_names: true)
+
+      expect(data[:errors]).to be_a(Array)
+      expect(data[:errors].first[:status]).to eq("404")
+      expect(data[:errors].first[:title]).to eq("Couldn't find Vendor with 'id'=123123123123")
+    end
   end
 end
