4778a0cfc5a4918d4abc5123243a70c7c13deaaf
Hoa Dam <hoangocdam@gmail.com>
2024-03-26 13:09:13 -0700

    add:update vendors routes

--- config/routes.rb
+++ config/routes.rb
@@ -11,7 +11,7 @@ Rails.application.routes.draw do
   namespace :api do
     namespace :v0 do
       resources :markets, only: :index
-      resources :vendors, only: [:show, :create]
+      resources :vendors, only: [:show, :create, :update]
     end
   end
 end
