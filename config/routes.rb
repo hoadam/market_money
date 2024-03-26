622674d42347762a77bdf59169bcb5fcac85a869
Laura Vega Vargas <ing.laurarvega@gmail.com>
2024-03-25 18:58:18 -0600

    specify to create routes for markets index only

--- config/routes.rb
+++ config/routes.rb
@@ -10,7 +10,7 @@ Rails.application.routes.draw do
 
   namespace :api do
     namespace :v0 do
-      resources :markets
+      resources :markets, only: :index
       resources :vendors
     end
   end
