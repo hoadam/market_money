db549f7e1413ad7ed070416cca4735673eb4ffd0
Hoa Dam <hoangocdam@gmail.com>
2024-03-25 21:03:06 -0700

    feat:test for validation

--- spec/models/vendor_spec.rb
+++ spec/models/vendor_spec.rb
@@ -2,7 +2,11 @@ require 'rails_helper'
 
 RSpec.describe Vendor, type: :model do
   describe "Validations" do
-
+    it {  should validate_presence_of :name }
+    it {  should validate_presence_of :description  }
+    it {  should validate_presence_of :contact_name }
+    it {  should validate_presence_of :contact_phone  }
+    it {  should validate_inclusion_of(:credit_accepted).in_array([true, false]) }
   end
 
   describe 'relationships' do
