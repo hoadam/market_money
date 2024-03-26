26d27a0d2d20a3f3c12acc4649a4f18a961ad502
Laura Vega Vargas <ing.laurarvega@gmail.com>
2024-03-26 16:01:13 -0600

    Create markets vendor route

--- config/routes.rb
+++ config/routes.rb
@@ -10,7 +10,9 @@ Rails.application.routes.draw do
 
   namespace :api do
     namespace :v0 do
-      resources :markets, only: [:index, :show]
+      resources :markets, only: [:index, :show] do
+        resources :vendors, only: :index
+      end
       resources :vendors, only: [:show, :create, :update]
     end
   end
