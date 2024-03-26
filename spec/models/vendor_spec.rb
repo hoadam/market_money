e8745e63ea1eeee6e45ce47115883f8ba6e7b0d5
Hoa Dam <hoangocdam@gmail.com>
2024-03-25 21:32:57 -0700

    feat:custom validation test for credit_accepted

--- spec/models/vendor_spec.rb
+++ spec/models/vendor_spec.rb
@@ -6,7 +6,14 @@ RSpec.describe Vendor, type: :model do
     it {  should validate_presence_of :description  }
     it {  should validate_presence_of :contact_name }
     it {  should validate_presence_of :contact_phone  }
-    it {  should validate_inclusion_of(:credit_accepted).in_array([true, false]) }
+    it "validates presence of credit_accepted" do
+      vendor = Vendor.new(name: 'AA', description: "abcd", contact_name: "John Doe", contact_phone: '123-45-7891')
+      expect(vendor).not_to be_valid
+      expect(vendor.errors[:credit_accepted]).to include("can't be nil")
+
+      vendor.credit_accepted = false
+      expect(vendor).to be_valid
+    end
   end
 
   describe 'relationships' do
