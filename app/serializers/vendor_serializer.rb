813d10ea90cd036210d23b704f10df385a831b96
Hoa Dam <hoangocdam@gmail.com>
2024-03-26 13:12:57 -0700

    delete unecessary format method

--- app/serializers/vendor_serializer.rb
+++ app/serializers/vendor_serializer.rb
@@ -5,21 +5,4 @@ class VendorSerializer
              :contact_name,
              :contact_phone,
              :credit_accepted
-
-  def self.format_vendors(vendors)
-    {
-      data: vendors.map do |vendor|
-        {
-          id: vendor.id,
-          attributes: {
-            name: vendor.name,
-            description: vendor.description,
-            contact_name: vendor.contact_name,
-            contact_phone: vendor.contact_phone,
-            credit_accepted: vendor.credit_accepted
-          }
-        }
-      end
-    }
-  end
 end
