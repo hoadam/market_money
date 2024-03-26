e4c92a794419baba30bc9c0b055605a02e5530a0
Laura Vega Vargas <ing.laurarvega@gmail.com>
2024-03-26 16:00:34 -0600

    Refactor

--- app/serializers/market_serializer.rb
+++ app/serializers/market_serializer.rb
@@ -11,33 +11,4 @@ class MarketSerializer
              :vendor_count
 
   has_many :vendors
-
-  # def self.format_market_data(market)
-  #   {
-  #     id: market.id,
-  #     type: "market",
-  #     attributes: market_attributes(market)
-  #   }
-  # end
-
-  # def self.format_markets(markets)
-  #   {
-  #     data: markets.map { |market| format_market_data(market) }
-  #   }
-  # end
-
-  # private
-  # def self.market_attributes(market)
-  #   {
-  #     name: market.name,
-  #     street: market.street,
-  #     city: market.city,
-  #     county: market.county,
-  #     state: market.state,
-  #     zip: market.zip,
-  #     lat: market.lat,
-  #     lon: market.lon,
-  #     vendor_count: market.vendor_count
-  #   }
-  # end
 end
