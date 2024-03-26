900db292eaa05784f28f8cf346e8c15f9b6244e1
Laura Vega Vargas <ing.laurarvega@gmail.com>
2024-03-26 11:59:07 -0600

    Refactor to fix id attribute out from attributes value

--- app/serializers/market_serializer.rb
+++ app/serializers/market_serializer.rb
@@ -1,7 +1,6 @@
 class MarketSerializer
   include JSONAPI::Serializer
-  attributes :id,
-             :name, 
+  attributes :name, 
              :street, 
              :city, 
              :county, 
@@ -11,33 +10,34 @@ class MarketSerializer
              :lon, 
              :vendor_count
 
-  def self.format_market_data(market)
-    {
-      id: market.id,
-      type: "market",
-      attributes: market_attributes(market)
-    }
-  end
+  has_many :vendors
 
-  def self.format_markets(markets)
-    {
-      data: markets.map { |market| format_market_data(market) }
-    }
-  end
+  # def self.format_market_data(market)
+  #   {
+  #     id: market.id,
+  #     type: "market",
+  #     attributes: market_attributes(market)
+  #   }
+  # end
 
-  private
+  # def self.format_markets(markets)
+  #   {
+  #     data: markets.map { |market| format_market_data(market) }
+  #   }
+  # end
 
-  def self.market_attributes(market)
-    {
-      name: market.name,
-      street: market.street,
-      city: market.city,
-      county: market.county,
-      state: market.state,
-      zip: market.zip,
-      lat: market.lat,
-      lon: market.lon,
-      vendor_count: market.vendor_count
-    }
-  end
+  # private
+  # def self.market_attributes(market)
+  #   {
+  #     name: market.name,
+  #     street: market.street,
+  #     city: market.city,
+  #     county: market.county,
+  #     state: market.state,
+  #     zip: market.zip,
+  #     lat: market.lat,
+  #     lon: market.lon,
+  #     vendor_count: market.vendor_count
+  #   }
+  # end
 end
