64e79151ea021ca53b8cd986763e678f02e9ac88
Laura Vega Vargas <ing.laurarvega@gmail.com>
2024-03-26 19:52:21 -0600

    Edge cases error handling testing added

--- spec/models/market_vendor_spec.rb
+++ spec/models/market_vendor_spec.rb
@@ -6,6 +6,7 @@ RSpec.describe MarketVendor, type: :model do
     @market_2 = create(:market)
     @vendor_1 = create(:vendor)
     @vendor_2 = create(:vendor)
+    @new_market_vendor = MarketVendor.new(market: @market_1,vendor: @vendor_1)
   end
 
   describe 'relationships' do
@@ -13,17 +14,33 @@ RSpec.describe MarketVendor, type: :model do
     it { should belong_to :vendor }
   end
 
-  describe 'instance_variables' do
+  describe 'validations' do
     describe 'valid_assoc' do
-      it 'creates error when market vendor already exists' do
+      it 'does not add an error when market vendor association does not already exist' do
+        
+        expect(@new_market_vendor.valid?).to be_truthy
+        expect(@new_market_vendor.errors).to be_empty
+      end
 
-        @existing_association = MarketVendor.create(market: @market_1,vendor: @vendor_1)
-      
-        @new_market_vendor = MarketVendor.new(market: @market_1,vendor: @vendor_1)
+      it 'adds an error for duplicate association when market vendor association already exists' do
+        MarketVendor.create(market: @market_1,vendor: @vendor_1)
+        
+        @new_market_vendor.valid?
+        expect(@new_market_vendor.errors[:base]).to include('Association already exists')
+      end
 
-        @new_market_vendor.valid_assoc
-      
-        expect(@new_market_vendor.errors[:base]).to include("Association already exists")
+      it 'adds an error for invalid market' do
+        @new_market_vendor.market_id = nil
+        @new_market_vendor.valid?
+
+        expect(@new_market_vendor.errors[:market_id]).to include("can't be blank")
+      end
+
+      it 'adds an error for invalid vendor' do
+        @new_market_vendor.vendor_id = nil
+        @new_market_vendor.valid?
+        
+        expect(@new_market_vendor.errors[:vendor_id]).to include("can't be blank")
       end
     end
   end
