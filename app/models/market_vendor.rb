cabba3158f4fd4b6b9904024d98f18c1d285841e
Laura Vega Vargas <ing.laurarvega@gmail.com>
2024-03-26 19:23:58 -0600

    Feat: create valid_assoc method for MarketVendor model

--- app/models/market_vendor.rb
+++ app/models/market_vendor.rb
@@ -1,4 +1,14 @@
 class MarketVendor < ApplicationRecord
   belongs_to :market
   belongs_to :vendor
+
+  validate :valid_assoc, on: :create
+
+  def valid_assoc
+    return unless market && vendor
+    
+    existing_association = MarketVendor.find_by(vendor_id: vendor.id, market_id: market.id)
+
+    errors.add(:base, 'Association already exists') if existing_association.present?
+  end
 end
