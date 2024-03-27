c4bab696860c7e3a5c8457b66b39b0c21d863bb6
Hoa Dam <hoangocdam@gmail.com>
2024-03-27 13:46:42 -0700

    feat:search method

--- app/controllers/api/v0/markets_controller.rb
+++ app/controllers/api/v0/markets_controller.rb
@@ -9,6 +9,16 @@ class Api::V0::MarketsController < ApplicationController
     render json: MarketSerializer.new(Market.find(params[:id]))
   end
 
+  def search
+    search_service = MarketSearchService.new(params)
+    if search_service.valid?
+      render json: MarketSerializer.new(search_service.search)
+
+    else
+      render json: { errors: [{ detail: "Invalid set of parameters. Please provide a valid set of parameters to perform a search with this endpoint."}  ] }, status: 422
+    end
+  end
+
   private
 
   def not_found_response(exception)
