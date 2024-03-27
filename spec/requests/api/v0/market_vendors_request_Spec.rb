4758a460744b5d42888a176f3ac0d5e4754fb48d
Laura Vega Vargas <ing.laurarvega@gmail.com>
2024-03-27 16:21:52 -0600

    Test: complete tests for happy and sad path for MarketVendors endpoints

--- spec/requests/api/v0/market_vendors_request_Spec.rb
+++ spec/requests/api/v0/market_vendors_request_Spec.rb
@@ -19,17 +19,40 @@ describe "Market Vendors Endpoints" do
 
   describe "Create market vendors" do
     it "creates a market vendor, sends a 201 status response detailing that a vendor was added to a market" do
-      post "/api/v0/market_vendors?market=#{@market_1.id}&vendor=#{@vendor_2.id}"
-      #/api/v0/markets/#{@market_1.id}/vendors"
+      post "/api/v0/market_vendors", params: {"market_id":"#{@market_1.id}", "vendor_id":"#{@vendor_2.id}"}
 
       expect(response).to be_successful
       expect(response.status).to eq(201)
-      expect(response.body).to eq("Vendor added to Market")
+      expect(JSON.parse(response.body)['message']).to eq("Vendor added to Market")
 
-      get "/api/v0/vendors"
+      get "/api/v0/markets/#{@market_1.id}/vendors"
+
+      market_vendors = JSON.parse(response.body, symbolize_names: true)[:data]
+
+      market_vendors.include?(@vendor_2)
     end
 
-    #Sad path
-    it ""
+    #Sad paths
+    it "sends a 404 status response when an invalid vendor id is passed in" do
+      post "/api/v0/market_vendors", params: {"market_id":"#{@market_1.id}", "vendor_id":"12341234"}
+
+      expect(response).not_to be_successful
+      expect(response.status).to eq(404)
+    end
+
+    it "sends a 404 status response when an invalid  market id is passed in" do
+      post "/api/v0/market_vendors", params: {"market_id":"12341234", "vendor_id":"#{@market_1.id}"}
+
+      expect(response).not_to be_successful
+      expect(response.status).to eq(404)
+    end
+
+    it "sends a 400 status response and detailed message when a vendor id and/or a market id are not passed in" do
+      post "/api/v0/market_vendors", params: {"market_id":"12341234"}
+
+      expect(response).not_to be_successful
+      expect(response.status).to eq(400)
+      expect(JSON.parse(response.body)['error']).to eq("Both market_id and vendor_id are required")
+    end
   end
 end
