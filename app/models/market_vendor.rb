b5fdc8d61805816f153d3e00ca465efbbdd2754c
Laura Vega Vargas <ing.laurarvega@gmail.com>
2024-03-26 19:52:51 -0600

    Edge cases error handling added to valid_assoc validation method in MarketVendor

--- app/models/market_vendor.rb
+++ app/models/market_vendor.rb
@@ -5,10 +5,12 @@ class MarketVendor < ApplicationRecord
   validate :valid_assoc, on: :create
 
   def valid_assoc
-    return unless market && vendor
-    
-    existing_association = MarketVendor.find_by(vendor_id: vendor.id, market_id: market.id)
+    errors.add(:market_id, "can't be blank") unless market_id.present?
+    errors.add(:vendor_id, "can't be blank") unless vendor_id.present?
 
-    errors.add(:base, 'Association already exists') if existing_association.present?
+    if market_id.present? && vendor_id.present?
+      existing_association = MarketVendor.find_by(vendor_id: vendor_id, market_id: market_id)
+      errors.add(:base, 'Association already exists') if existing_association
+    end
   end
 end
